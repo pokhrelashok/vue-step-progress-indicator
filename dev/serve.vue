d8098a870405e19b4bea8c5c1be6c52a2a6cceb7
pokhrelashok <pokhrelashok2@gmail.com>
2021-03-27 21:47:10 +0545

    Project init

---
+++ dev/serve.vue
@@ -0,0 +1,20 @@
+<script>
+import Vue from "vue";
+import VueCircularStepProgressIndicator from "@/vue-circular-step-progress-indicator.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    VueCircularStepProgressIndicator,
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-circular-step-progress-indicator
+      :steps="['1', '2', '3', '4']"
+      activeStep="0"
+    />
+  </div>
+</template>
---
+++ src/vue-circular-step-progress-indicator.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="row ml-1">
+    <div class="mb-4 progress-holder">
+      <span v-for="(step, index) in steps" :key="'step_' + step"
+        ><div
+          :class="
+            'progress-bubble ' + (index == activeStep ? 'active-step' : '')
+          "
+        >
+          {{ index + 1 }}
+        </div>
+        <span class="progress-label ml-1 mr-4">{{ step }}</span></span
+      >
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "ProgressBar",
+  props: {
+    steps: {
+      type: Array,
+      required: true,
+    },
+    activeStep: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+
+
+<style scoped>
+.ml-1 {
+  margin-left: 0.25rem !important;
+}
+.mr-4 {
+  margin-right: 1.5rem !important;
+}
+.mb-4 {
+  margin-bottom: 1.5rem !important;
+}
+.row {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+}
+.progress-holder {
+  line-height: 30px;
+}
+.progress-bubble {
+  display: inline-block;
+  height: 30px;
+  width: 30px;
+  border-radius: 100%;
+  background: transparent;
+  border: 1px grey solid;
+  text-align: center;
+}
+.active-step {
+  border: 1px solid #1e7e34;
+}
+</style>
